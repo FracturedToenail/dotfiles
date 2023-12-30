a31de7b3dfe84a6a24414e47a0e8d0e1966c4fd7
FracturedToenail <zamoldileo@gmail.com>
2023-12-30 17:29:59 +1000

    added .config

---
+++ .config/doom/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("/Users/leozamoldinov/org/MAIN.org" "/Users/leozamoldinov/org/Daily Things.org" "/Users/leozamoldinov/org/Chopsticks Plan.org" "/Users/leozamoldinov/org/Doom Emacs Important Commands.org" "/Users/leozamoldinov/org/SOMEDAY.org" "/Users/leozamoldinov/org/Thom Yorke's Order.org" "/Users/leozamoldinov/org/checklist.org" "/Users/leozamoldinov/org/improvements.org" "/Users/leozamoldinov/org/minecraft.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
